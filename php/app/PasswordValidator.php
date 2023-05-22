dbb4cb4955584daa2e28c9653b33383017ec24c2
johnopleda <johno@realtair.com>
2023-05-22 14:17:43 +0800

    Update test

--- php/app/PasswordValidator.php
+++ php/app/PasswordValidator.php
@@ -2,29 +2,18 @@
 
 namespace App;
 
-class PasswordValidator {
+class PasswordValidator
+{
 
     private $password;
 
-    public function __construct($password) {
+    public function __construct($password)
+    {
         $this->password = $password;
     }
 
-    /**
-     * This function takes a string password and validates it against a set of validation rules.
-     *
-     * Validation rule messages:
-     *  - Password must be at least 6 characters long
-     *  - Password must include at least one number
-     *  - Password must include at least one uppercase letter
-     *  - Password must include at least one lowercase letter
-     *  - Password must include at least one special character
-     *
-     * @return array An array of error messages for any validation rule that the password does not meet.
-     */
-    public function validate() {
-        $errors = [];
-        
-        return $errors;
+    public function validate()
+    {
+        return false;
     }
 }
